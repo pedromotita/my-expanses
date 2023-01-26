b23a3b908d62b97e51ad85a7afbf02e137b87098
Pedro Mota <p.smota98@gmail.com>
2023-01-26 17:27:19 -0300

    Added expanses table view

--- MyExpanses/MonthExpansesDetail/MonthExpansesDetailViewController.swift
+++ MyExpanses/MonthExpansesDetail/MonthExpansesDetailViewController.swift
@@ -12,11 +12,17 @@ class MonthExpansesDetailViewController: UIViewController {
     weak var coordinator: MainCoordinator?
     
     private var balanceLabel = UILabel()
+    private var expansesTableView = UITableView(frame: .zero, style: .insetGrouped)
+    private var headerLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .systemGray6
+        
         configureBalanceLabel()
+        configureExpansesTableView()
+        configureHeaderLabel()
     }
     
     private func configureBalanceLabel() {
@@ -32,4 +38,43 @@ class MonthExpansesDetailViewController: UIViewController {
         
         balanceLabel.textAlignment = .center
     }
+    
+    private func configureHeaderLabel() {
+        view.addSubview(headerLabel)
+        
+        headerLabel.text = "Expanses"
+        headerLabel.textColor = .black
+        headerLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        headerLabel.topAnchor.constraint(equalTo: balanceLabel.bottomAnchor, constant: 32).isActive = true
+        headerLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
+    }
+    
+    private func configureExpansesTableView() {
+        view.addSubview(expansesTableView)
+        
+        expansesTableView.delegate = self
+        expansesTableView.dataSource = self
+        
+        expansesTableView.rowHeight = 43.5
+        
+        expansesTableView.translatesAutoresizingMaskIntoConstraints = false
+        expansesTableView.topAnchor.constraint(equalTo: balanceLabel.bottomAnchor, constant: 32).isActive = true
+        expansesTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        expansesTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        expansesTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
+extension MonthExpansesDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
