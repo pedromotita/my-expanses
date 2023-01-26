4718bab7e13ac6659f2ef2e0fd5adced091f9865
Pedro Mota <p.smota98@gmail.com>
2023-01-26 17:12:13 -0300

    Added balance label

--- MyExpanses/MonthExpansesDetail/MonthExpansesDetailViewController.swift
+++ MyExpanses/MonthExpansesDetail/MonthExpansesDetailViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 class MonthExpansesDetailViewController: UIViewController {
     
     weak var coordinator: MainCoordinator?
-
+    
+    private var balanceLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemGray6
+        configureBalanceLabel()
     }
-
+    
+    private func configureBalanceLabel() {
+        view.addSubview(balanceLabel)
+        
+        balanceLabel.text = "R$25,00"
+        balanceLabel.textColor = .black
+        balanceLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        
+        balanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        balanceLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 32).isActive = true
+        
+        balanceLabel.textAlignment = .center
+    }
 }
