dfe0da259f8f2bd68ec36d99da82999f55dea0b1
Pedro Mota <p.smota98@gmail.com>
2023-01-26 16:03:45 -0300

    Added expanse detail view with blue background

---
+++ MyExpanses/MonthExpansesDetail/MonthExpansesDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MonthExpansesDetailViewController.swift
+//  MyExpanses
+//
+//  Created by Pedro Mota on 26/01/23.
+//
+
+import UIKit
+
+class MonthExpansesDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
